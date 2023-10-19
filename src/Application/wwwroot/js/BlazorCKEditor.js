6e9433b793db8e82d2f00afaaf91a484e544b2d9
Jai Paragas <iaj.paragas@yahoo.com>
2023-10-19 08:33:30 +0800

    Change CKEditor to Classic,add HTML source support

--- src/Application/wwwroot/js/BlazorCKEditor.js
+++ src/Application/wwwroot/js/BlazorCKEditor.js
@@ -1,40 +1,54 @@
 (function () {
-    window.CKEditorFunctions = {
-        getCkEditorContent: function (ckEditor) {
-            return ckEditor.getData();
+  window.CKEditorFunctions = {
+    getCkEditorContent: function (ckEditor) {
+      return ckEditor.getData();
+    },
+    createEditor: function (editorElement) {
+      ClassicEditor.create(editorElement, {
+        htmlSupport: {
+          allow: [
+            {
+              name: "div",
+              styles: true,
+            },
+            {
+              name: "img",
+              styles: true,
+            },
+          ],
         },
-        createEditor: function (editorElement) {
-            DecoupledEditor
-                .create(editorElement, {})
-                .then(editor => {
-                    window.editor = editor;
-                    document.querySelector('.document-editor__toolbar').appendChild(editor.ui.view.toolbar.element);
-                    document.querySelector('.ck-toolbar').classList.add('ck-reset_all');
-                    editor.model.document.on('change:data', () => {
-                        const htmlValue = editor.getData();
-                        window.Blazor.updateHtmlValue(htmlValue);
-                    });
-                })
-                .catch(error => {
-                    window.CKEditorFunctions.handleSampleError(error);
-                });
-        },
-        handleSampleError: function (error) {
-            const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
-            const message = [
-                'Oops, something went wrong!',
-                `Please, report the following error on ${issueUrl} with the build id "pobufn5bauzq-6g7qzajounyp" and the error stack trace:`
-            ].join('\n');
-            console.error(message);
-            console.error(error);
-        }
-    };
+      })
+        .then((editor) => {
+          window.editor = editor;
+          document
+            .querySelector(".document-editor__toolbar")
+            .appendChild(editor.ui.view.toolbar.element);
+          document.querySelector(".ck-toolbar").classList.add("ck-reset_all");
+          editor.model.document.on("change:data", () => {
+            const htmlValue = editor.getData();
+            window.Blazor.updateHtmlValue(htmlValue);
+          });
+        })
+        .catch((error) => {
+          window.CKEditorFunctions.handleSampleError(error);
+        });
+    },
+    handleSampleError: function (error) {
+      const issueUrl = "https://github.com/ckeditor/ckeditor5/issues";
+      const message = [
+        "Oops, something went wrong!",
+        `Please, report the following error on ${issueUrl} with the build id "pobufn5bauzq-6g7qzajounyp" and the error stack trace:`,
+      ].join("\n");
+      console.error(message);
+      console.error(error);
+    },
+  };
 
-    window.registerUpdateHtmlValueFunction = function (dotnetHelper) {
-        window.Blazor = {
-            updateHtmlValue: function (htmlValue) {
-                dotnetHelper.invokeMethodAsync('UpdateHtmlValue', htmlValue);
-            }
-        };
+  window.registerUpdateHtmlValueFunction = function (dotnetHelper) {
+    window.Blazor = {
+      updateHtmlValue: function (htmlValue) {
+        dotnetHelper.invokeMethodAsync("UpdateHtmlValue", htmlValue);
+      },
     };
+  };
 })();
